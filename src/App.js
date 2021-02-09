5def7f0e648c15bc2348ca8c533180cf208cbb27
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2021-02-10 02:35:05 +0530

    added static html skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React,{useState} from 'react';
+import Result from './Result';
 
 function App() {
+  const [currentResult,setResult]=useState(
+    ' '
+  )
+  const setResultHandler=()=>{
+    setResult(`The Result of the roll is ${Math.floor(Math.random()*5+1)}`)
+  }
+
   return (
     <div className="App">
-      
+      <Result value={currentResult} />
+      <button onClick={setResultHandler}>Roll The Dice</button>
     </div>
   );
 }
---
+++ src/Result.js
@@ -0,0 +1,8 @@
+const Result=(props)=>{
+    return(
+        <div >
+            <p>{props.value}</p>
+        </div>
+    )
+}
+export default Result;
