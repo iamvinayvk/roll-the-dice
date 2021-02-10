6a22d44a86d5cac768e3db45977fefaf81db0573
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2021-02-11 01:35:49 +0530

    added rolling dice from react-dice-roll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 import React,{useState} from 'react';
 import Result from './Result';
+import diceImage from './dice.gif';
+import Dice from 'react-dice-roll';
+
 
 function App() {
+  var diceValue=6;
   const [currentResult,setResult]=useState(
     ' '
   )
   const setResultHandler=()=>{
-    setResult(`The Result of the roll is ${Math.floor(Math.random()*5+1)}`)
+    setTimeout(
+      ()=>{
+    setResult(`The Result of the roll is ${diceValue}`);}
+      ,1000)
+    ;
+    // return diceValue;
   }
 
   return (
     <div className="App">
+        
+       <button onClick={setResultHandler}><Dice  onRoll={(value)=>{
+         diceValue=value;
+       }} rollingTime={1000} size={50} />
       <Result value={currentResult} />
-      <button onClick={setResultHandler}>Roll The Dice</button>
+      {/* <button onClick={setResultHandler}>Roll The Dice</button> */}
+      </button>
+      {/* <button onClick={setResultHandler}>Roll The Dice</button> */}
     </div>
   );
 }
